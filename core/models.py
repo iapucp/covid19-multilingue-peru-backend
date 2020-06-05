7e2d9a7b0f157a8d5fe881cb11aee3699c14dfe7
Daniel Matos <daniel.matos@tuxpas.com>
2020-06-05 13:38:30 -0500

    Add sample language model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from pynamodb.attributes import UnicodeAttribute
+from pynamodb.models import Model
 
-# Create your models here.
+
+class Language(Model):
+
+    class Meta:
+        table_name = "core_languages"
+        host = "http://db:8000"
+        read_capacity_units = 4
+        write_capacity_units = 4
+
+    code = UnicodeAttribute(hash_key=True)
+    name = UnicodeAttribute()
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from core.models import Language
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class LanguageTestCase(SimpleTestCase):
+    """
+    SimpleTestCase is used to avoid default Django database connection.
+
+    setUp and tearDown methods include some basic table rename to separate from
+    dev environment
+    """
+
+    def setUp(self):
+        Language.Meta.table_name = "test_" + Language.Meta.table_name
+        Language.create_table()
+        Language('AC', name='ACHUAR').save()
+        Language('QU', name='QUECHUA').save()
+
+    def test_query_language(self):
+        """
+        Just a sample test c:
+        """
+        language = Language.get('AC')
+        self.assertEqual(language.name, 'ACHUAR')
+
+    def tearDown(self):
+        Language.delete_table()
